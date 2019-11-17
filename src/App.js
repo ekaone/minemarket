e5496e11241ecd9635f3a1f33eff667efcc94cc0
Eka <ekaone3033@gmail.com>
2019-11-17 20:35:15 +0700

    👀

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
 import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import {
-  Dashboard,
-  NavigationDrawer,
-  PermanentDrawer,
-  MarryDoe,
-  JohnDoe,
-  Batman,
-  Jocker,
-  BlackLabel,
-  JohnnyWalker
-} from './components'
+import Apps from './Apps'
 
 function App() {
-
-  const theme = createMuiTheme({
-    palette: {
-      type: "dark"
-    }
-  });
-
   return (
-    <>
-    {/* <MuiThemeProvider theme={theme}>
-    <CssBaseline /> */}
-        
-      <BlackLabel />
-        
-    {/* </MuiThemeProvider> */}
-    </>
+    <Apps />
   );
 }
 
---
+++ src/Apps/Apps.js
@@ -0,0 +1,24 @@
+import React from 'react'
+// import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+import {
+  Dashboard,
+  NavigationDrawer,
+  PermanentDrawer,
+  MarryDoe,
+  JohnDoe,
+  Batman,
+  Jocker,
+  BlackLabel,
+  JohnnyWalker
+} from '../components'
+
+function Apps() {
+  return (
+    <div>
+      <BlackLabel />
+    </div>
+  )
+}
+
+export default Apps
